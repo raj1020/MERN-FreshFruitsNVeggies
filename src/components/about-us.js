35c979457148ca547dd0b70e0993e17da9d69498
raj1020 <raj.1020@hotmail.com>
2020-04-23 20:59:05 +1000

    initial commit for Fresh Fruits and Veggies

---
+++ src/components/about-us.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class AboutUs extends Component {
+
+    render () {
+        <div>
+            <p> This is the about us page for the online fruits and veggies business.</p>
+        </div>
+    }
+    
+}
